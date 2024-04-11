fdf8ecbe04e281ae38a9febfa79f95f606aa2d02
dgodoy1996 <danielagdytcr@yahoo.com>
2024-04-11 11:17:26 -0400

    Add 2 reviews models

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Movie, Show
+from .models import Movie, Show, MovieReview, ShowReview
 
 # Register your models here.
 admin.site.register(Movie)
 admin.site.register(Show)
+admin.site.register(MovieReview)
+admin.site.register(ShowReview)
